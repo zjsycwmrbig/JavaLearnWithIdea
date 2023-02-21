98628abc496d9fc870dbf98c52c9d2bd110e8432
zjsycwmrbig <2057544748@qq.com>
2023-02-21 22:07:07 +0800

    fir commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
 //        test1();
-
+        System.out.println(Student.SEX);
     }
     public static void test1() {
         var Mynum = new Test1();
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,5 @@
 public class Student {
+    public static  String SEX = "0 and 1";
     public static  void PrintSchool(){
         System.out.println("BUPT!");
     }
